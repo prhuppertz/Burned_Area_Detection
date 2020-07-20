1f45f74c18acfab323b4c300ea504efb603f8641
prhuppertz <prhuppertz@gmail.com>
2020-07-20 11:19:46 +0100

    changed cache status for dvc push of outputs

--- repro/generate_dataset.sh
+++ repro/generate_dataset.sh
@@ -2,7 +2,7 @@ dvc run -n merge_bands \
 -d preparedata/merge_bands.py \
 -d raw_data/s2_aws/tiles/31 \
 -d raw_data/s2_aws/tiles/30 \
--O data/scenes \
+-o data/scenes \
 "python preparedata/merge_bands.py /home/robert/ds-wildfire/raw_data/s2_aws/tiles -s 31/T/GH -s 31/T/GJ /home/robert/ds-wildfire/data/scenes"
 
 dvc run -n patch_scenes \
@@ -11,19 +11,19 @@ dvc run -n patch_scenes \
 -d preparedata/patching.py \
 -d raw_data/wildfires-ground-truth/france/vars/N_DFCI_CONTOUR_FEUX_2017_S_083.shp \
 -d data/scenes/ \
--O data/extracted/ \
+-o data/extracted/ \
 "python preparedata/extract_patches.py /home/robert/ds-wildfire/raw_data/wildfires-ground-truth/france/vars/N_DFCI_CONTOUR_FEUX_2017_S_083.shp /home/robert/ds-wildfire/data/extracted DATE_ECLOS /home/robert/ds-wildfire/data/scenes -s 31/T/GJ -s 31/T/GH"
 
 dvc run -n sort_patches \
 -d preparedata/sort_patches.py \
 -d data/extracted/ \
--O data/training_patches/anno/ \
--O data/training_patches/patches/ \
+-o data/training_patches/anno/ \
+-o data/training_patches/patches/ \
 "python preparedata/sort_patches.py /home/robert/ds-wildfire/data/training_patches /home/robert/ds-wildfire/excluded.txt /home/robert/ds-wildfire/data/extracted/patches /home/robert/ds-wildfire/data/extracted/anno"
 
 dvc run -n split_data \
 -d preparedata/split.py \
 -d data/training_patches/patches/ \
 -d data/training_patches/anno/ \
--O data/training_patches/training_indices.json \
+-o data/training_patches/training_indices.json \
 "python preparedata/split.py --root=data/training_patches/"
