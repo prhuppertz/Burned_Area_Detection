09dfd633631045cbdaa59a4465fc702721fec47b
prhuppertz <prhuppertz@gmail.com>
2020-07-29 11:45:08 +0100

    modified for Scenario2b

--- repro/generate_dataset.sh
+++ repro/generate_dataset.sh
@@ -9,19 +9,19 @@ dvc run -n patch_scenes \
 -d preparedata/patching.py \
 -d raw_data/wildfires-ground-truth/portugal/AArdida2016_ETRS89PTTM06_20190813.shp \
 -d data/scenes/ \
--o data/post_fire_model_Scenario2/extracted/ \
-"python preparedata/extract_patches.py /home/robert/ds-wildfire/raw_data/wildfires-ground-truth/portugal/AArdida2016_ETRS89PTTM06_20190813.shp /home/robert/ds-wildfire/data/post_fire_model_Scenario2/extracted DHFim /home/robert/ds-wildfire/data/scenes -s 29/S/MC -s 29/S/MD -s 29/S/NB -s 29/S/NC -s 29/S/ND -s 29/S/PB -s 29/S/PC -s 29/S/PD -s 29/T/ME -s 29/T/NE -s 29/T/NF -s 29/T/NG -s 29/T/PE -s 29/T/PF"
+-o data/post_fire_model_Scenario2b/extracted/ \
+"python preparedata/extract_patches.py /home/robert/ds-wildfire/raw_data/wildfires-ground-truth/portugal/AArdida2016_ETRS89PTTM06_20190813.shp /home/robert/ds-wildfire/data/post_fire_model_Scenario2b/extracted DHFim /home/robert/ds-wildfire/data/scenes -s 29/S/MC -s 29/S/MD -s 29/S/NB -s 29/S/NC -s 29/S/ND -s 29/S/PB -s 29/S/PC -s 29/S/PD -s 29/T/ME -s 29/T/NE -s 29/T/NF -s 29/T/NG -s 29/T/PE -s 29/T/PF"
 
 dvc run -n sort_patches \
 -d preparedata/sort_patches.py \
--d data/post_fire_model_Scenario2/extracted/ \
--o data/post_fire_model_Scenario2/training_patches/annotations/ \
--o data/post_fire_model_Scenario2/training_patches/patches/ \
-"python preparedata/sort_patches.py /home/robert/ds-wildfire/data/post_fire_model_Scenario2/training_patches /home/robert/ds-wildfire/excluded.txt /home/robert/ds-wildfire/data/post_fire_model_Scenario2/extracted/patches /home/robert/ds-wildfire/data/post_fire_model_Scenario2/extracted/annotations"
+-d data/post_fire_model_Scenario2b/extracted/ \
+-o data/post_fire_model_Scenario2b/training_patches/annotations/ \
+-o data/post_fire_model_Scenario2b/training_patches/patches/ \
+"python preparedata/sort_patches.py /home/robert/ds-wildfire/data/post_fire_model_Scenario2b/training_patches /home/robert/ds-wildfire/excluded.txt /home/robert/ds-wildfire/data/post_fire_model_Scenario2b/extracted/patches /home/robert/ds-wildfire/data/post_fire_model_Scenario2b/extracted/annotations"
 
 dvc run -n split_data \
 -d preparedata/split.py \
--d data/post_fire_model_Scenario2/training_patches/patches/ \
--d data/post_fire_model_Scenario2/training_patches/annotations/ \
--o data/post_fire_model_Scenario2/training_patches/training_indices.json \
-"python preparedata/split.py --root=data/post_fire_model_Scenario2/training_patches/"
+-d data/post_fire_model_Scenario2b/training_patches/patches/ \
+-d data/post_fire_model_Scenario2b/training_patches/annotations/ \
+-o data/post_fire_model_Scenario2b/training_patches/training_indices.json \
+"python preparedata/split.py --root=data/post_fire_model_Scenario2b/training_patches/"
--- repro/resnetunet.sh
+++ repro/resnetunet.sh
@@ -1,11 +1,11 @@
 dvc run -fn resnetunet \
--d data/post_fire_model_Scenario2/training_patches/patches/ \
--d data/post_fire_model_Scenario2/training_patches/training_indices.json \
--d data/post_fire_model_Scenario2/training_patches/annotations/ \
+-d data/post_fire_model_Scenario2b/training_patches/patches/ \
+-d data/post_fire_model_Scenario2b/training_patches/training_indices.json \
+-d data/post_fire_model_Scenario2b/training_patches/annotations/ \
 -d segmentation/evaluation/ \
 -d run_training.py \
 -d segmentation/models/resnetunet/ \
 -d segmentation/networks/resnetunet/ \
 -d segmentation/data/ \
-"python run_training.py --seed=11 --gpu=1 --save-images=1 --baseline=1 --model-name=resnetunet --group=resnetunet_Scenario2 --save-path=data/post_fire_model_Scenario2/"
+"python run_training.py --seed=11 --gpu=1 --save-images=1 --baseline=1 --model-name=resnetunet --group=resnetunet_Scenario2b --save-path=data/post_fire_model_Scenario2b/"
 
