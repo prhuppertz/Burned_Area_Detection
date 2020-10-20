45512128824ff4bc7df4a16db819052fee7690f4
prhuppertz <prhuppertz@gmail.com>
2020-10-20 14:48:50 +0100

    edited merge_bands with refactoring and renaming of variables

--- src/preparedata/merge_bands.py
+++ src/preparedata/merge_bands.py
@@ -16,57 +16,96 @@ import glob
 from tqdm import tqdm
 from docopt import docopt
 from datetime import datetime
+from pathlib import Path
 
-# selecting bands that are needed for the task at hand
-SELECTED_BANDS = ["B12", "B8A", "B03"]
+
+def create_source_paths(list_of_bands, path_to_scene):
+    """
+    Creates a dict of paths to all bands in path_to_scene
+    :param list_of_bands: List of bands
+    :param path_to_scene: path to the mgrs scene
+    """
+    bands_string=""
+    dict_source_paths = {}
+    meta_source = {}
+    for band in list_of_bands:
+        # create dictionary with paths to all files of each band
+        dict_source_paths[band] = glob.glob(
+            path_to_scene + "/*/*/*/*/" + band + "_sur.tif"
+        )
+        # store the metadata of one image of each band
+        with rasterio.open(dict_source_paths[band][0]) as opened_band:
+            #store the meta data of the opened band in the dictionary
+            meta_source[band] = opened_band.meta
+            #update the count to the number of bands
+            meta_source[band].update(count=len(list_of_bands))
+        bands_string+=band
+        
+    return dict_source_paths, bands_string, meta_source
+        
+def create_target_paths(dict_source_paths, save_path, mgrs_coordinate, bands_string, list_of_bands):
+    
+    """
+    Creates a dict of target paths by date and bands
+    :param list_of_bands: List of bands
+    :param list_of_paths: List of all paths that belong to a certain band
+    
+    :param paths_target: path to the mgrs scene
+    """
+    dict_target_paths = {}
+    date = {}
+    dict_of_dates={}
+    # create date list for the selected scene, create target files for stacking images
+    
+    for path in dict_source_paths[list_of_bands[0]]:
+        date_index = path.index("201")
+        date_end_index = path.index("/0")
+        #create dict with source paths and dates
+        dict_of_dates[path] = path[date_index:date_end_index]
+        #create target path with date and bands_string
+        os.makedirs(os.path.join(save_path, mgrs_coordinate, dict_of_dates[path]), exist_ok=True)
+        #create dict with dates and target paths
+        dict_target_paths[dict_of_dates[path]] = os.path.join(
+            save_path, mgrs_coordinate, dict_of_dates[path], bands_string + ".tif"
+        )
+    return dict_target_paths
+
+def write_bands(dict_source_paths, meta_source, dict_target_paths, list_of_bands):
+    """
+    Writes bands to target path
+    :param list_of_bands: List of bands
+    :param list_of_paths: List of all paths that belong to a certain band
+    :param paths_target: path to the mgrs scene
+    """
+    list_target_paths=list(dict_target_paths.values())
+    for x in list_target_paths:
+        with rasterio.open(x, "w", **meta_source[list_of_bands[0]]) as target_file:
+            for band in list_of_bands:
+                with rasterio.open(dict_source_paths[band][list_target_paths.index(x)]) as source_file:
+                    #write the source file into the target file's band in the order of the 
+                    target_file.write_band((list_of_bands.index(band) + 1), source_file.read(1))
 
 
 def stack_bands(source_path, mgrs_coordinate, save_path):
     """
-    Reads from source and stacks image bands into a single raster file
-    :param source_path: Path to image source
-    :param mgrs_coordinate: MGRS coordinate in path format, for example 31/U/FQ
+    Stacks bands
+    :param source_path: Path to source images
+    :param mgrs_coordinates: List of MGRS coordinates, for example ['31/U/FQ', '31/U/FO']
     :param save_path: Directory where a stacked raster would be saved
-    :return:
     """
-    # initialisation of lists
-    list_of_paths = {}
-    date = {}
-    path_target = {}
-    meta_source = {}
-    BANDS = ""
+
+    #selecting the SWIR, NIR and Green band names from the remote sensing dataset
+    dict_of_bands = {"SWIR-band":"B12","NIR-band":"B8A","Green-band":"B03"}
+    list_of_bands = list(dict_of_bands.values())
 
     path_to_scene = os.path.join(source_path, mgrs_coordinate)
 
-    for n in range(0, len(SELECTED_BANDS)):
-        # create lists of paths to the processed images for all existing dates for each SELECTED_BAND
-        list_of_paths[n] = glob.glob(
-            path_to_scene + "/*/*/*/*/" + SELECTED_BANDS[n] + "_sur.tif"
-        )
-        # store the metadata of one BAND and adapt the length to the SELECTED_BANDS
-        with rasterio.open(list_of_paths[n][0]) as src0:
-            meta_source[n] = src0.meta
-            meta_source[n].update(count=len(SELECTED_BANDS))
-        # create string with the selected bands
-        BANDS += SELECTED_BANDS[n]
+    dict_source_paths, bands_string, meta_source = create_source_paths(list_of_bands, path_to_scene)
 
-    # create date list for the selected scene, create target files for stacking images
-    for i in range(0, len(list_of_paths[0])):
-        date_index = list_of_paths[0][i].index("201")
-        date_end_index = list_of_paths[0][i].index("/0")
-        date[i] = list_of_paths[0][i][date_index:date_end_index]
-        os.makedirs(os.path.join(save_path, mgrs_coordinate, date[i]), exist_ok=True)
-        path_target[i] = os.path.join(
-            save_path, mgrs_coordinate, date[i], BANDS + ".tif"
-        )
-
-    # write SELECTED_BANDS into the target files
-    for y in range(0, len(list_of_paths[0])):
-        with rasterio.open(path_target[y], "w", **meta_source[0]) as dst:
-            for x in range(0, len(SELECTED_BANDS)):
-                with rasterio.open(list_of_paths[x][y]) as src1:
-                    dst.write_band((x + 1), src1.read(1))
-
+    dict_target_paths = create_target_paths(dict_source_paths, save_path, mgrs_coordinate, bands_string, list_of_bands)
+    
+    write_bands(dict_source_paths, meta_source, dict_target_paths, list_of_bands)
+    
 
 def process_scenes(source_path, mgrs_coordinates, save_path):
     """
@@ -76,8 +115,8 @@ def process_scenes(source_path, mgrs_coordinates, save_path):
     :param save_path: Directory where a stacked raster would be saved
     """
 
-    for mgrs in mgrs_coordinates:
-        stack_bands(source_path, mgrs, save_path)
+    for mgrs_coordinate in mgrs_coordinates:
+        stack_bands(source_path, mgrs_coordinate, save_path)
 
 
 if __name__ == "__main__":
